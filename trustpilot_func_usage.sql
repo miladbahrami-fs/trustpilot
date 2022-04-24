f63a7f787924ddcce772cd485df984dbec8ceb15
Milad <milad@Milads-MacBook-Pro.local>
2022-04-24 14:49:59 +0430

    updating function usage

--- trustpilot_func_usage.sql
+++ trustpilot_func_usage.sql
@@ -1,9 +1,9 @@
-DECLARE start_date STRING DEFAULT '2022-02-01';
-DECLARE end_date STRING DEFAULT '2022-02-02';
+DECLARE start_date DATE DEFAULT '2022-04-01';
+DECLARE end_date DATE DEFAULT '2022-04-02';
 DECLARE invitation_limit INT64 DEFAULT 300;
 DECLARE interval_days INT64 DEFAULT 90;
-DECLARE metric STRING DEFAULT 'bo_turnover_usd';
+DECLARE metric STRING DEFAULT 'bo_pnl_usd';
 DECLARE percentage_threshold INT64 DEFAULT 5;
 DECLARE threshold FLOAT64 DEFAULT 1000;
-DECLARE first_time_meet BOOL DEFAULT TRUE; 
-CALL `business-intelligence-240201.development.trustpilot_invitation_func`(start_date, end_date, invitation_limit, interval_days, metric, percentage_threshold, threshold, first_time_meet);
+DECLARE first_time_meet BOOL DEFAULT FALSE; 
+CALL `business-intelligence-240201.development.sp_get_trustpilot_invitation_list`(start_date, end_date, invitation_limit, interval_days, metric, percentage_threshold, threshold, first_time_meet);
